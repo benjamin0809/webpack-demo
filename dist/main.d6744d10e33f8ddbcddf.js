2d07bb3875cecb2b991e013a011674afc91adfe8
Benjamin Chiu <894306909@qq.com>
2022-02-09 19:24:45 +0800

    add dist

--- dist/main.d6744d10e33f8ddbcddf.js
+++
@@ -1,1 +0,0 @@
-!function(){"use strict";var n=document.createElement("div"),e=document.createElement("button");e.innerHTML="main button";var t=document.getElementById("input");e.onclick=function(){n.innerHTML="Benjamin Chiu"+t.value+Math.random(),document.body.appendChild(n)},document.body.appendChild(e),console.log11(88888)}();
